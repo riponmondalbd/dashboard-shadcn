5295bb6637a18728600554a1a754de07528d4a6c
riponmondalbd <riponmondal149@gmail.com>
2025-07-13 01:05:44 +0600

    make dropdown menu

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,6 +1,14 @@
-import { Moon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, Moon, Settings, User } from "lucide-react";
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 
 export default function Navbar() {
   return (
@@ -11,10 +19,32 @@ export default function Navbar() {
       <div className="flex items-center gap-4">
         <Link href={"/"}>Dashboard</Link>
         <Moon />
-        <Avatar>
-          <AvatarImage src="https://avatars.githubusercontent.com/u/58094597?s=400&u=a41c33c3968bac28de0523e673864bf6204238c9&v=4" />
-          <AvatarFallback>Ripon</AvatarFallback>
-        </Avatar>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            {" "}
+            <Avatar>
+              <AvatarImage src="https://avatars.githubusercontent.com/u/58094597?s=400&u=a41c33c3968bac28de0523e673864bf6204238c9&v=4" />
+              <AvatarFallback>Ripon</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10} align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
