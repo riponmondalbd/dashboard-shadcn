4d8a1f4b9d5bf36f55aebc9d3cc88d2d544ceb24
riponmondalbd <riponmondal149@gmail.com>
2025-07-12 22:06:30 +0600

    shadcn custom theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { CirclePlus } from "lucide-react";
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Button variant={"destructive"} size={"xl"}>
+      <Button size={"xl"} className="text-custom-color">
         <CirclePlus />
         Click Me
       </Button>
