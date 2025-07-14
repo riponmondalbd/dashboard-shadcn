2e324f5c165505149ff30b975f42b84307dc2216
riponmondalbd <riponmondal149@gmail.com>
2025-07-15 04:18:47 +0600

    make advance pagination

--- src/app/payments/data-table.tsx
+++ src/app/payments/data-table.tsx
@@ -8,7 +8,7 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 
-import { Button } from "@/components/ui/button";
+import { DataTablePagination } from "@/components/TablePagination";
 import {
   Table,
   TableBody,
@@ -78,7 +78,7 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      {/* <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="outline"
           size="sm"
@@ -95,7 +95,8 @@ export function DataTable<TData, TValue>({
         >
           Next
         </Button>
-      </div>
+      </div> */}
+      <DataTablePagination table={table} />
     </div>
   );
 }
